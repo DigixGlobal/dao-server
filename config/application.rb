434e90d37088b4387e4ff535149d9c10bfd3ecc1
mrenoon <vunguyenvyvp@gmail.com>
2018-10-24 16:09:20 +0800

    add seeding for database (Needed for DGDG-102)

--- config/application.rb
+++ config/application.rb
@@ -33,21 +33,7 @@ module DaoServer
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    def add_user(address, uid)
-      return if User.find_by(address: address)
-      u = User.new(address: address, uid: uid)
-      u.save
-    end
-
-    config.after_initialize do
-      add_user('0x68911e512a4ecbd12d5dbae3250ff2c8e5850b60', '01')
-      add_user('0x300ac2c15a6778cfdd7eaa6189a4401123ff9dda', '02')
-      add_user('0x602651daaea32f5a13d9bd4df67d0922662e8928', '03')
-      add_user('0x9210ddf37582861fbc5ec3a9aff716d3cf9be5e1', '04')
-      add_user('0xe02a693f038933d7b28301e6fb654a035385652d', '05')
-      add_user('0xcbe85e69eec80f29e9030233a757d49c68e75c8d', '06')
-      add_user('0x355fbd38b3219fa3b7d0739eae142acd9ea832a1', '07')
-    end
+    
 
 
   end
