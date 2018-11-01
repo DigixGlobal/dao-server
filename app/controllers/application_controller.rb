e97641082cbec991daee3e2f0be336cea04f71b4
mrenoon <vunguyenvyvp@gmail.com>
2018-11-01 12:33:35 +0800

    add endpoints for pending transactions (Needed for DGDG-120)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def error_response(error='Error')
+    render json: { error: error }
+  end
 end
