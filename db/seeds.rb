614a51a43bb6f254096742f46a38b31748513aba
Vu <vunguyenvyvp@gmail.com>
2018-11-02 16:10:48 +0800

    add ganache addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,5 +26,28 @@ add_user('0xe02a693f038933d7b28301e6fb654a035385652d', '05')
 add_user('0xcbe85e69eec80f29e9030233a757d49c68e75c8d', '06')
 add_user('0x355fbd38b3219fa3b7d0739eae142acd9ea832a1', '07')
 
+
+
+add_user('0x9f244f9316426030bca51baf35a4541422ab4f76', '08')
+add_user('0x0b2b99eb6850df81452df017d278f97d26426ace', '09')
+
+add_user('0x5d1e440153966c5ab576457f702a1778e27d44c7', '10')
+
+add_user('0x508221f68118d1eaa631d261aca3f2fccc6ecf91', '11')
+
+add_user('0x519774b813dd6de58554219f16c6aa8350b8ec99', '12')
+
+add_user('0xca731a9a354be04b8ebfcd9e429f85f48113d403', '13')
+
+add_user('0x1a4d420bff04e68fb76096ec3cbe981f509c3341', '14')
+
+
+add_user('0x11ad4d13bcca312e83eec8f961ada76c41c0ef09', '15')
+
+add_user('0xad127e217086779bc0a03b75adee5f5d729aa4eb', '16')
+
+add_user('0x0d4f271e282ddcc7290ad3569458d2c399f34eb6', '17')
+
+
 add_nonce('self', 0)
 add_nonce('infoServer', 0)
