ff3453ce746f63af3656652a7a1d3b6a68617d0e
Rohit Narurkar <rohitnarurkar@protonmail.com>
2018-11-08 16:08:16 +0800

    add txns that are already completed (Needed for DGDG-120)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,28 +2,16 @@ class UserController < ApplicationController
 
   def details
     authenticate_user!
-    puts 'authenticated'
-    puts "current user = #{current_user}"
     render json: current_user
   end
 
   def new_user
-    message = request.method() + request.original_fullpath + request.raw_post + request.headers["ACCESS-NONCE"]
-
-    digest = OpenSSL::Digest.new('sha256')
-    computedSig = OpenSSL::HMAC.hexdigest(digest, SERVER_SECRET, message)
-
-    currentNonce = Nonce.find_by(server: 'infoServer')
-    retrievedNonce = Integer(request.headers["ACCESS-NONCE"])
-
-    body = JSON.parse(request.raw_post)
-
-    if computedSig === request.headers["ACCESS-SIGN"] && retrievedNonce > currentNonce.nonce
+    if verify_info_server_request(request)
+      currentNonce = Nonce.find_by(server: 'infoServer')
+      retrievedNonce = Integer(request.headers["ACCESS-NONCE"])
       Nonce.update(currentNonce.id, :nonce => retrievedNonce)
-      userAddress = body["address"]
-      uid = get_new_uid
-      u = User.new(address: userAddress, uid: uid)
-      u.save
+      body = JSON.parse(request.raw_post)
+      add_new_user(body["address"])
 
       render json: { status: 200, msg: "correct" }
     else
@@ -32,6 +20,14 @@ class UserController < ApplicationController
   end
 
   private
+  def is_unique_uid(uid)
+    if User.find_by(uid: uid)
+      return false
+    else
+      return true
+    end
+  end
+
   def get_new_uid
     uid = Random.rand(1000000)
     while (!is_unique_uid(uid)) do
@@ -40,11 +36,9 @@ class UserController < ApplicationController
     return uid
   end
 
-  def is_unique_uid(uid)
-    if User.find_by(uid: uid)
-      return false
-    else
-      return true
-    end
+  def add_new_user(userAddress)
+    uid = get_new_uid
+    u = User.new(address: userAddress, uid: uid)
+    u.save
   end
 end
