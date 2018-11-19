884de5a5ce77dbf5ec49cfc88d05757eac710e26
Anthony Eufemio <ace@digix.global>
2018-11-19 07:26:53 -0800

    capistrano staging configs (Needed for DGDG-159)

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,42 +4,12 @@ lock "~> 3.11.0"
 set :application, "dao_server"
 set :repo_url, "git@github.com:DigixGlobal/dao-server.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-
-# TODO
-set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :deploy_to, "/home/appuser/apps/dao_server"
 set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
-
-# TODO
 set :user, 'appuser'
-
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :puma_bind,       "tcp://127.0.0.1:3000"
@@ -53,6 +23,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :rvm_type, :user                     # Defaults to: :auto
 set :rvm_ruby_version, '2.5.0@daoserver'
 
+
 set :puma_conf, "#{shared_path}/config/puma.rb"
 
 namespace :deploy do
@@ -60,9 +31,3 @@ namespace :deploy do
   after 'puma:smart_restart', 'nginx:restart'
   after 'deploy', 'puma:restart'
 end
-
-
-
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
