9f3629fbfd31c7a28f5bd20d2d3fa31472f0837e
Francis Murillo <francismurillo@digix.global>
2018-11-22 13:23:57 +0800

    Add address validation

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   sequence(:uid) { |_| Random.srand }
-  sequence(:address) { |_| format('0x%040d', Random.srand) }
+  sequence(:address) { |_| format('0xa%039d', Random.srand) }
 
   factory :user, class: 'User' do
     uid { generate(:uid) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,4 +16,25 @@ class UserTest < ActiveSupport::TestCase
     assert_not User.new(params).save,
                'should fail with the same data'
   end
+
+  test 'user address should validate correctly' do
+    user = create(:user)
+    address = generate(:address)
+
+    assert user.update(address: address),
+           'should be valid'
+    assert user.update(address: address.upcase.sub('X', 'x')),
+           'should be case insensitive'
+
+    assert_not user.update(address: ''),
+               'should not accept empty'
+
+    assert_not user.update(address: address + 'a'),
+               'should not exceed 42 characters'
+    assert_not user.update(address: address.slice(1)),
+               'should not fall below 42 characters'
+
+    assert_not user.update(address: 'g' + address.slice(2) + 'G'),
+               'should only accept hexadecimal characters'
+  end
 end
