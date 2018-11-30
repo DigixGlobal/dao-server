84a919f0a3ae04900e462b9ee35b8610daa81728
mrenoon <vunguyenvyvp@gmail.com>
2018-11-30 16:35:15 +0800

    add scripts for setting up systems

---
+++ scripts/pull.sh
@@ -0,0 +1,19 @@
+cd ../governance-ui-components
+git pull
+git checkout develop
+
+cd ../governance-ui
+git pull
+git checkout develop
+
+cd ../dao-server
+git pull
+git checkout dev
+
+cd ../info-server
+git pull
+git checkout dev
+
+cd ../dao-contracts
+git pull
+git checkout dev
---
+++ scripts/restart.sh
@@ -0,0 +1,31 @@
+pm2 del info-server:dev
+
+if [ $SKIP_DAO_CONTRACTS!=true ]
+then
+  cd ../dao-contracts
+  npm run dao:dev
+  truffle exec scripts/send-dummy-tnxs.js > /dev/null &
+fi
+
+if [ $SKIP_DAO_SERVER!=true ]
+then
+  cd ../dao-server
+  bin/rails db:drop
+  bin/rails db:create
+  bin/rails db:migrate
+  bin/rails db:seed
+  bin/rails s > /dev/null &
+fi
+
+if [ $SKIP_INFO_SERVER!=true ]
+then
+  cd ../info-server
+  service mongod restart
+  npm run dev
+fi
+
+if [ $SKIP_UI!=true ]
+then
+  cd ../governance-ui
+  npm start
+fi
