35acebceff66a41e2fd1e9ab54a22c7e0c723174
Francis Murillo <francismurillo@digix.global>
2019-02-07 18:27:29 +0800

    [fix] Check for https with use_ssl

--- lib/ethereum_api.rb
+++ lib/ethereum_api.rb
@@ -23,7 +23,7 @@ class EthereumApi
       uri = URI.parse(SERVER_URL)
       print 'uri to request infura = ', uri, ' uri.port = ', uri.port, '\n'
       https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = Rails.env.production?
+      https.use_ssl = uri.host.starts_with?('https')
 
       puts 'uri.path = ', uri.path
 
