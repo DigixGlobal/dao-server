a9d02f3ef8442fbc0d2aff5e5583a9f3d832f854
Francis Murillo <francismurillo@digix.global>
2019-02-07 20:28:20 +0800

    Copy prod file as staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,11 @@
-set :stage, :staging
-server 'info01-dev', user: 'appuser', roles: %w{app web db}
+# frozen_string_literal: true
 
-set :default_env, {
-  histfile: "/dev/null",
-  rails_env: "staging",
-  dao_staging_database_password: ENV['DAO_STAGING_DATABASE_PASSWORD'],
-  secret_key_base: ENV['DAO_STAGING_SECRET_KEY_BASE'],
-  info_server_url: ENV['INFO_SERVER_URL']
-}
+set :stage, :staging
+server 'dao-staging', user: 'appuser', roles: %w[app web db]
+
+set :default_env,
+    histfile: '/dev/null',
+    rails_env: 'staging',
+    dao_staging_database_password: ENV['DAO_STAGING_DATABASE_PASSWORD'],
+    secret_key_base: ENV['DAO_STAGING_SECRET_KEY_BASE'],
+    info_server_url: ENV['INFO_SERVER_URL']
