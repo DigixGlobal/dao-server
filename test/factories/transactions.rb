5eba22ca51f67414fb4f19dc2b001b35793bb24b
Francis Murillo <francismurillo@digix.global>
2019-02-27 09:02:46 +0800

    [feature] Add type and project fields when creating transactions

--- test/factories/transactions.rb
+++ test/factories/transactions.rb
@@ -6,11 +6,24 @@ FactoryBot.define do
   sequence(:title) { |_| Faker::StarWars.quote }
   sequence(:txhash) { |_| "0x#{Faker::Ethereum.address}" }
   sequence(:block_number) { |_| SecureRandom.random_number(10_000) }
+  sequence(:type) { |_| Transaction.types.keys.sample }
 
   factory :transaction, class: 'Transaction' do
     title { generate(:title) }
     txhash { generate(:txhash) }
     block_number { generate(:block_number) }
     association :user, factory: :user
+
+    factory :transaction_claim_result do
+      transaction_type { |_| 1 }
+      project { generate(:address) }
+    end
+  end
+
+  factory :claim_result_transaction, class: 'Object' do
+    title { generate(:title) }
+    txhash { generate(:txhash) }
+    type { |_| 1 }
+    project { generate(:address) }
   end
 end
