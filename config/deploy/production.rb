50991993beb68dc895b7aaeef5b4e90c839c537a
mrenoon <vunguyenvyvp@gmail.com>
2019-03-06 16:00:35 +0800

    add production variables

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,9 +2,9 @@
 
 set :default_env,
     histfile: '/dev/null',
-    rails_env: 'staging',
-    dao_staging_database_password: ENV['DAO_STAGING_DATABASE_PASSWORD'],
-    dao_staging_secret_key_base: ENV['DAO_STAGING_SECRET_KEY_BASE'],
+    rails_env: 'production',
+    dao_staging_database_password: ENV['DAO_PRODUCTION_DATABASE_PASSWORD'],
+    secret_key_base: ENV['DAO_PRODUCTION_SECRET_KEY_BASE'],
     info_server_url: ENV['INFO_SERVER_URL'],
     postmark_api_token: ENV['POSTMARK_API_TOKEN']
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 set :stage, :staging
-server 'info01-dev', user: 'appuser', roles: %w[app web db]
+server 'dao-staging', user: 'appuser', roles: %w[app web db]
 
 set :default_env,
     histfile: '/dev/null',
@@ -9,6 +9,6 @@ set :default_env,
     dao_staging_database_password: ENV['DAO_STAGING_DATABASE_PASSWORD'],
 
     secret_key_base: ENV['DAO_STAGING_SECRET_KEY_BASE'],
-    info_server_url: ENV['INFO_SERVER_URL']
+    info_server_url: ENV['INFO_SERVER_URL'],
 
     postmark_api_token: ENV['POSTMARK_API_TOKEN']
