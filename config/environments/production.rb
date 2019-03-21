95bf1494caf824ba8b466af8b554914d97c3e414
mrenoon <vunguyenvyvp@gmail.com>
2019-03-21 11:10:36 +0800

    fix abi issue on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.action_cable.allowed_request_origins = [%r{*}]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
