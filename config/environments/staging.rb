74abf4d0a6e3ac187518326bcf2c3538a20ff426
mrenoon <vunguyenvyvp@gmail.com>
2019-04-09 11:34:55 +0800

    allow all origins

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -74,7 +74,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.action_cable.allowed_request_origins = ['https://www-kovan.digixdev.com']
+  # config.action_cable.allowed_request_origins = ['https://www-kovan.digixdev.com']
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
