4322fdf67949b2ca8787d1ae03fd5f9de094a1ec
mrenoon <vunguyenvyvp@gmail.com>
2019-04-11 11:18:26 +0800

    add config changes for staging and production

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 set :stage, :staging
-server 'info01-dev', user: 'appuser', roles: %w[app web db]
+server 'dao-staging', user: 'appuser', roles: %w[app web db]
 
 set :default_env,
     histfile: '/dev/null',
     rails_env: 'staging',
     dao_staging_database_password: ENV['DAO_STAGING_DATABASE_PASSWORD'],
-    dao_staging_secret_key_base: ENV['DAO_STAGING_SECRET_KEY_BASE'],
+
+    secret_key_base: ENV['DAO_STAGING_SECRET_KEY_BASE'],
     info_server_url: ENV['INFO_SERVER_URL'],
+
+    postmark_from: ENV['POSTMARK_FROM'],
     postmark_api_token: ENV['POSTMARK_API_TOKEN'],
     whitelist_ips: ENV['WHITELIST_IPS']
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -73,7 +73,8 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+
+  config.action_cable.allowed_request_origins = ['https://www-kovan.digixdev.com']
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
